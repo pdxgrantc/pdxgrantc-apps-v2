c83cea2b004b525388d8ac86a9d36da64506f27c
pdxgrantc <pdxgrantc@gmail.com>
2023-01-25 14:46:39 -0800

    laptop

--- shopping-list/src/Pages/Home/Home.js
+++ shopping-list/src/Pages/Home/Home.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 // Firebase
-import { auth, signInWithGoogle } from '../../firebase';
+import { auth, signInWithGoogle, db } from '../../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { getDocs, collection } from "firebase/firestore";
 
 // Partials
 import Header from '../Static/Partials/Header/Header';
-import LeftBar from './Partials/LeftBar';
 import NewList from './Partials/NewList';
+import FindFriends from './Partials/FindFriends';
 
+var pageToggle = false;
 
 export default function Home() {
     const [user] = useAuthState(auth);
@@ -36,7 +38,7 @@ function SignedIn() {
                 <LeftBar />
             </div>
             <div class="w-full bg-black">
-                <NewList />
+                {pageToggle ? <FindFriends /> : <NewList />}
             </div>
         </>
     )
@@ -58,3 +60,110 @@ function SignedOut() {
         </>
     )
 }
+
+function LeftBar() {
+    return (
+        <div class="bg-black h-full flex flex-col">
+            <div class="px-[1.5vw] py-[3vh]">
+                <h2 class="text-[2.75rem]">Your Stuff</h2>
+                <div class="h-[1vh]"></div>
+                <Lists />
+                <div class="h-[3vh]"></div>
+                <Friends />
+            </div>
+        </div>
+    )
+}
+
+function Lists() {
+    if (window.location.pathname === "/") {
+        return (
+            <div>
+                <h3 class="text-[1.75rem] font-semibold">Your Lists</h3>
+                <div class="text-[1.5rem]">
+                    <YourLists />
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <h3 class="text-[1.75rem] font-semibold">Your Lists</h3>
+                <div class="text-[1.5rem]">
+                    <YourLists />
+                    <div class="h-[.75vh]"></div>
+                    <h4 class="w-fit text-2xl border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[1.25vw] py-[.5vh]">Create New List</h4>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+function YourLists() {
+    const [lists, setLists] = React.useState([])
+
+    React.useEffect(() => {
+        const getYourLists = async () => {
+            const lists = []
+            const querySnapshot = await getDocs(collection(db, "users", auth.currentUser.email, "lists"))
+            querySnapshot.forEach((doc) => {
+                lists.push(doc.data())
+                console.log(doc.data())
+            })
+            setLists(lists)
+        }
+        getYourLists()
+    }, [])
+
+    // map lists by last_edited_at 
+    lists.sort((a, b) => {
+        return b.last_edited_at - a.last_edited_at
+    })
+
+    return (
+        <div class="flex flex-col text-[1.25rem]">
+
+            {lists.map((list) => (
+                <p>{list.list_title}</p>
+            ))}
+        </div>
+    )
+}
+
+function changeToggle() {
+    if (pageToggle) {
+        pageToggle = false
+    }
+    else {
+        pageToggle = true;
+    }
+}
+
+function Friends() {
+    return (
+        <div>
+            <h3 class="text-[1.75rem]">Your Friends</h3>
+            <div class="text-[1.5rem]">
+                <YourFriends />
+                <div class="h-[.75vh]"></div>
+                <button onclick={changeToggle} class="cursor-pointer w-fit text-2xl border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[1.25vw] py-[.5vh]">Find Friends</button>
+            </div>
+        </div>
+    )
+}
+
+function YourFriends() {
+    return (
+        <div class="flex flex-col text-[1.25rem]">
+            <p>Friend Item</p>
+            <p>Friend Item</p>
+            <p>Friend Item</p>
+            <p>Friend Item</p>
+            <p>Friend Item</p>
+        </div>
+    )
+}
+
+// autocomplete https://www.telerik.com/blogs/quick-guide-dropdown-menus-react
