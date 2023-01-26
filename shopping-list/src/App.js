431e9dc76742f163bb026266445cdb055adf6c58
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-01-26 14:02:56 -0800

    school

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import MyLists from './Pages/MyLists/MyLists';
+import List from './Pages/List/List';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/mylists' element={<MyLists />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path='/my-lists' element={<MyLists />} />
+        <Route path='/my-lists/:listName' element={<List />} />
+        <Route path='*' element={<h1>404 - Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ shopping-list/src/Pages/List/List.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+// firebase
+import { auth, db } from '../../firebase'
+import { collection, query, where, getDoc, getDocs } from 'firebase/firestore'
+
+// component that takes a list name as a parameter
+export default function List() {
+
+    // get the list name from the url
+    const location = useLocation()
+    const listName = location.pathname.split('/')[2]
+
+    return (
+        <div>
+            <h1>{listName}</h1>
+        </div>
+    )
+}
