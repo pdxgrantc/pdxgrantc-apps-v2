6fac2be910cf1653f4a7aa984a05c3db8ae0fba8
pdxgrantc <pdxgrantc@gmail.com>
2023-01-31 08:44:39 -0800

    more advanced list page

--- shopping-list/src/Pages/List/List.js
+++ shopping-list/src/Pages/List/List.js
@@ -42,10 +42,50 @@ export default function List() {
             </Helmet>
             <div class="m-auto bg-main_bg_color text-text_white min-h-screen">
                 <Header />
-                <div>
-                    <h1>{listName}</h1>
-                    <h2>{listDescription}</h2>
+                <div class="h-[5vh] min-h-[25px]"></div>
+                <div class="w-[88%] m-auto bg-black px-[3%] py-[3vh]">
+                    <div>
+                        <h1 class="text-[3.5rem] font-semibold">{listName}</h1>
+                        <h2 class="text-[2.25rem]">{listDescription}</h2>
+                    </div>
+                    <div class="h-[2.5vh] min-h-[15px]"></div>
+                    <Items />
+                    <div class="h-[2.5vh] min-h-[15px]"></div>
+                    <AddList />
+                    <div></div>
                 </div>
+            </div>
+        </>
+    )
+}
+
+
+function Items() {
+    return (
+        <></>
+    )
+}
+
+function AddList() {
+    const [listItemName, setListTitle] = useState('')
+    const [listItemCost, setListItemCost] = useState('')
+    return (
+        <>
+            <div class="flex justify-between gap-[30px]">
+                <h3 class="text-[2.25rem] whitespace-nowrap leading-10">New Item</h3>
+                <input class="text-black w-[50vw] h-[5vh] text-[1.5rem] border-[1.5px] border-black focus:outline-none px-2"
+                    type='text'
+                    placeholder='Item Name'
+                    value={listItemName}
+                    onChange={(e) => setListTitle(e.target.value)}
+                />
+                <input class="text-black w-[50vw] h-[5vh] text-[1.5rem] border-[1.5px] border-black focus:outline-none px-2"
+                    type='text'
+                    placeholder='Item Cost'
+                    value={listItemCost}
+                    onChange={(e) => setListItemCost(e.target.value)}
+                />
+                <button class="cursor-pointer w-fit text-2xl border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[1.25vw] py-[.5vh]">Add</button>
             </div>
         </>
     )
