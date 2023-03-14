088502b242240e2e98d9d3159a8509e9b64f9117
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-14 14:51:53 -0700

    cocktails created

---
+++ cocktails/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLTy_kRlmiqmKW40ou6CJfEXLZ5RayBcg",
+  authDomain: "cocktails-f4d1a.firebaseapp.com",
+  projectId: "cocktails-f4d1a",
+  storageBucket: "cocktails-f4d1a.appspot.com",
+  messagingSenderId: "269267686622",
+  appId: "1:269267686622:web:ef7cd41e81d042891c9e4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
