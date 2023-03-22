453bc13f416babc124083c1f99b8582c626e6d41
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-22 16:57:58 -0700

    first effort

--- cocktails/src/Ingredients/MyItems.js
+++ cocktails/src/Ingredients/MyItems.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import Collapse from "react-collapse";
 
 // Firebase
-import { db } from '../firebase'
+import { db, auth } from '../firebase'
 import { doc, getDoc } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
-export default function MyItems(props) {
-    const user = props.user;
+export default function MyItems() {
+    const [user] = useAuthState(auth)
 
     const [userDoc, setUserDoc] = useState(null)
 
@@ -24,8 +26,93 @@ export default function MyItems(props) {
         getUserDoc()
     }, [user])
 
+    if (!userDoc) {
+        return (
+            <div className='text-[2rem] font-semibold'>
+                Loading...
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='on_desktop:grid on_desktop:grid-cols-2'>
+                <Garnish garnish={userDoc.garnish} />
+            </div>
+        )
+    }
+}
+
+
+function Garnish(props) {
+    const [isOpen, setIsOpen] = useState(true);
+    const garnish = props.garnish
+
+    if (props.garnish) {
+        return (
+            <div className="">
+                <button
+                    className='text-[2.5rem] font-semibold pb-[.25rem]'
+                    onClick={() => setIsOpen(!isOpen)}>
+                    Garnish
+                </button>
+                <Collapse isOpened={isOpen} className="">
+                    <div className='pl-5'>
+                        <Fruit fruit={garnish.fruit} />
+                        <Other />
+                    </div>
+                </Collapse>
+            </div>
+        );
+    }
+}
+
+
+function Fruit(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+        
+    if (props.fruit) {
+        console.log(props.fruit)
+        return (
+            <div className='cursor-pointer rounded-[4px]'>
+                <button
+                    className='text-[2rem] font-ssemibold'
+                    onClick={() => setIsOpen(!isOpen)}>
+                    Fruit
+                </button>
+                <Collapse isOpened={isOpen} className="">
+                    <div className='grid grid-cols-2 pl-3'>
+                        <p>Oranges</p>
+                        <p>Lemons</p>
+                        <p>Limes</p>
+                        <p>Cherries</p>
+                    </div>
+                </Collapse>
+            </div>
+        )
+    }
+}
+
+function Other() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div>
+        <div className='cursor-pointer rounded-[4px]'>
+            <button
+                className='text-[2rem] font-ssemibold'
+                onClick={() => setIsOpen(!isOpen)}>
+                Other
+            </button>
+            <Collapse isOpened={isOpen} className="">
+                <div className='grid grid-cols-2 pl-3'>
+                    <p>Salt</p>
+                    <p>Olives</p>
+                    <p>Mint</p>
+                    <p>Celery</p>
+                    <p>Pickles</p>
+                    <p>Whipped Cream</p>
+                </div>
+            </Collapse>
         </div>
     )
 }
